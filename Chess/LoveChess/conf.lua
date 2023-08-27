ace5cecd22408b248769684e4921038bf043a618
byronlove-lace <byronlovelace@proton.me>
2023-08-27 21:35:21 +0700

    distances attempt

--- Chess/LoveChess/conf.lua
+++ Chess/LoveChess/conf.lua
@@ -3,10 +3,11 @@
 function love.conf(t)
         t.version = "11.4"
         t.window.title = "Chess"
-        t.window.width = 640
-        t.window.height = 640
+        t.window.width = 720
+        t.window.height = 720
         t.window.vsync = 1
         t.window.display = 1
         t.window.fullscreen = false
+        t.window.borderless = true
 end
 
--- Chess/LoveChess/main.lua
+++ Chess/LoveChess/main.lua
@@ -4,14 +4,29 @@ _G.love = require("love")
 
 function love.load()
         board = {
-                image = love.graphics.newImage('sprites/board.png')
+                image = love.graphics.newImage('sprites/board.png'),
+                border = 7
         }
         pieces = {
                 black_pawn = {
-                image = love.graphics.newImage('sprites/black_pawn2.png'),
+                image = love.graphics.newImage('sprites/black_pawn.png'),
                 first = {
-                        x = 20,
-                        y = 20
+                        x = 7 + (88.25 * 1) + 15.5,
+                        y = 7 + (88.25 * 1) + 15.5
+                },
+                second = {},
+                third = {},
+                fourth = {},
+                fifth = {},
+                sixth = {},
+                seventh = {},
+                eight = {}
+                },
+                white_pawn = {
+                image = love.graphics.newImage('sprites/white_pawn.png'),
+                first = {
+                        x = (90 * 6) + 15.5,
+                        y = (90 * 6) + 15.5
                 },
                 second = {},
                 third = {},
@@ -23,6 +38,8 @@ function love.load()
                 }
         }
         selector = {
+                x = 0,
+                y = 0
         }
 end
 
@@ -31,10 +48,11 @@ function love.update(dt)
 end
 
 function love.draw()
-        love.graphics.draw(board.image)
+        love.graphics.draw(board.image, 0, 0)
         love.graphics.setColor(70/255, 200/255, 235/255, 1)
-        love.graphics.rectangle("fill", 5, 5, 80, 80)
+        love.graphics.rectangle("fill", 7, 7, 90, 90)
         love.graphics.setColor(1, 1, 1, 1)
         love.graphics.draw(pieces.black_pawn.image, pieces.black_pawn.first.x, pieces.black_pawn.first.y)
+        love.graphics.draw(pieces.white_pawn.image, pieces.white_pawn.first.x, pieces.white_pawn.first.y)
 end
 
